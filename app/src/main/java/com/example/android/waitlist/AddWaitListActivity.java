533bd05bdf8aca6196f7089aabc733cdb6d89f01
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-05-24 16:38:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/waitlist/AddWaitListActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.waitlist;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+
+public class AddWaitListActivity extends AppCompatActivity {
+    private SQLiteDatabase mDb;
+    private EditText mNewGuestNameEditText;
+    private EditText mNewPartySizeEditText;
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addname);
+        mNewGuestNameEditText = (EditText) this.findViewById(R.id.person_name_edit_text);
+        mNewPartySizeEditText = (EditText) this.findViewById(R.id.party_count_edit_text);
+        WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
+        mDb = dbHelper.getWritableDatabase();
+    }
+    public void addToWaitlist(View view) {
+        if (mNewGuestNameEditText.getText().length() == 0 ||
+                mNewPartySizeEditText.getText().length() == 0) {
+            return;
+        }
+        //default party size to 1
+        int partySize = 1;
+        try {
+            //mNewPartyCountEditText inputType="number", so this should always work
+            partySize = Integer.parseInt(mNewPartySizeEditText.getText().toString());
+        } catch (NumberFormatException ex) {
+            Log.e(LOG_TAG, "Failed to parse party size text to number: " + ex.getMessage());
+        }
+
+        // Add guest info to mDb
+        addNewGuest(mNewGuestNameEditText.getText().toString(), partySize);
+
+        // Update the cursor in the adapter to trigger UI to display the new list
+
+        //clear UI text fields
+        mNewPartySizeEditText.clearFocus();
+        mNewGuestNameEditText.getText().clear();
+        mNewPartySizeEditText.getText().clear();
+        Intent intent = new Intent(AddWaitListActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+    private long addNewGuest(String name, int partySize) {
+        ContentValues cv = new ContentValues();
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE, partySize);
+        return mDb.insert(WaitlistContract.WaitlistEntry.TABLE_NAME, null, cv);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.android.waitlist;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.ColorInt;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    private GuestListAdapter mAdapter;
+    private SQLiteDatabase mDb;
+    private EditText mNewGuestNameEditText;
+    private EditText mNewPartySizeEditText;
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+    RecyclerView waitlistRecyclerView;
+
+    @ColorInt
+    private int backgroundColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Set local attributes to corresponding views
+        waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
+        mNewGuestNameEditText = (EditText) this.findViewById(R.id.person_name_edit_text);
+        mNewPartySizeEditText = (EditText) this.findViewById(R.id.party_count_edit_text);
+
+        // Set layout for the RecyclerView, because it's a list we are using the linear layout
+        waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        // Create a DB helper (this will create the DB if run for the first time)
+        WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
+
+        // Keep a reference to the mDb until paused or killed. Get a writable database
+        // because you will be adding restaurant customers
+        mDb = dbHelper.getWritableDatabase();
+
+        // Get all guest info from the database and save in a cursor
+        Cursor cursor = getAllGuests();
+
+        // Create an adapter for that cursor to display the data
+        mAdapter = new GuestListAdapter(this, cursor);
+
+        // Link the adapter to the RecyclerView
+        waitlistRecyclerView.setAdapter(mAdapter);
+
+        // COMPLETED (3) Create a new ItemTouchHelper with a SimpleCallback that handles both LEFT and RIGHT swipe directions
+        // Create an item touch helper to handle swiping items off the list
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            // COMPLETED (4) Override onMove and simply return false inside
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                //do nothing, we only care about swiping
+                return false;
+            }
+
+            // COMPLETED (5) Override onSwiped
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // COMPLETED (8) Inside, get the viewHolder's itemView's tag and store in a long variable id
+                //get the id of the item being swiped
+                long id = (long) viewHolder.itemView.getTag();
+                // COMPLETED (9) call removeGuest and pass through that id
+                //remove from DB
+                removeGuest(id);
+                // COMPLETED (10) call swapCursor on mAdapter passing in getAllGuests() as the argument
+                //update the list
+                mAdapter.swapCursor(getAllGuests());
+            }
+
+
+            //COMPLETED (11) attach the ItemTouchHelper to the waitlistRecyclerView
+        }).attachToRecyclerView(waitlistRecyclerView);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.addButton:
+                Intent AddIntent = new Intent(this,AddWaitListActivity.class);
+                startActivity(AddIntent);
+            case R.id.settings:
+
+
+       }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    /**
+     * This method is called when user clicks on the Add to waitlist button
+     *
+     * @param view The calling view (button)
+     */
+
+
+
+    /**
+     * Query the mDb and get all guests from the waitlist table
+     *
+     * @return Cursor containing the list of guests
+     */
+    private Cursor getAllGuests() {
+        return mDb.query(
+                WaitlistContract.WaitlistEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
+        );
+    }
+
+    /**
+     * Adds a new guest to the mDb including the party count and the current timestamp
+     *
+     * @param name  Guest's name
+     * @param partySize Number in party
+     * @return id of new record added
+     */
+
+
+
+    // COMPLETED (1) Create a new function called removeGuest that takes long id as input and returns a boolean
+    /**
+     * Removes the record with the specified id
+     *
+     * @param id the DB id to be removed
+     * @return True: if removed successfully, False: if failed
+     */
+    private boolean removeGuest(long id) {
+        // COMPLETED (2) Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id
+        return mDb.delete(WaitlistContract.WaitlistEntry.TABLE_NAME, WaitlistContract.WaitlistEntry._ID + "=" + id, null) > 0;
+    }
+
+
+}
